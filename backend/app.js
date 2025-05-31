e6cfd0093feec6bdfdf3c67974acad48fbfd4c5c
bassem <182443885+bassemsaeed@users.noreply.github.com>
2025-05-31 18:10:24 +0300

    Add global dotenv Config & Impl search endpoint & ratelimiter
    added global dotenv configuration to make it easy to use env variables
    in any file within ll directories. Also, implemented the search endpoint
    to be able to search any thing, be it a character, series or movies
    the result contain a collection of all types of data that matches the query.
    Also added a ratelimiter middleware to not make anyone abuse the server since its public
    for any one to interact with through any http agent

--- backend/app.js
+++ backend/app.js
@@ -1,8 +1,23 @@
+import "./config/dotenv.config.js"; // initialize environment vars
 import express from "express";
-import { getTrendingController } from "./controllers/trending.controller.js";
+import {
+  getTrendingController,
+  searchController,
+} from "./controllers/trending.controller.js";
+import { rateLimit } from "express-rate-limit";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  limit: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes).
+  standardHeaders: "draft-8", // `RateLimit-*` headers
+  legacyHeaders: false, // no X-RateLimit headers 
+});
+
+
+app.use(limiter);
 
 app.get("/", (req, res) => {
   res.send("<h1 style='color: red;'>Hello world</h1>");
@@ -10,7 +25,7 @@ app.get("/", (req, res) => {
 
 app.get("/trending", getTrendingController);
 
-
+app.get("/search", searchController);
 
 app.listen(PORT, () => {
   console.log("running on http://localhost:3000");
