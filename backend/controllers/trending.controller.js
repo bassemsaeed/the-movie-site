1e2e6d7fcdfc1354423e09a3b29180b2d18d2a5d
bassem <182443885+bassemsaeed@users.noreply.github.com>
2025-05-31 18:10:24 +0300

    Add global dotenv Config & Impl search endpoint & ratelimiter
    added global dotenv configuration to make it easy to use env variables
    in any file within ll directories. Also, implemented the search endpoint
    to be able to search any thing, be it a character, series or movies
    the result contain a collection of all types of data that matches the query.
    Also added a ratelimiter middleware to not make anyone abuse the server since its public
    for any one to interact with through any http agent

--- backend/controllers/trending.controller.js
+++ backend/controllers/trending.controller.js
@@ -1,4 +1,4 @@
-import { getTrending } from "../services/tmdb.service.js";
+import { getTrending, searchMulti } from "../services/tmdb.service.js";
 
 async function getTrendingController(req, res) {
   const { k, l } = req.query;
@@ -28,8 +28,22 @@ async function getTrendingController(req, res) {
   }
 }
 
+async function searchController(req, res) {
+  const { q, l, page } = req.query;
+  const validPageNum = page ?? "1";
+  
+  // check if correct lang choice, if not return an error and terminate function execution
+  if (l !== "ar" && l !== "en" && l !== undefined) {
+    res.json({
+      status: "Error",
+      cause: "Unkown language choice -> " + l,
+    });
 
+    return;
+  }
 
+  const data = await searchMulti(q, l, validPageNum);
+  res.status(200).json(data);
+}
 
-
-export {getTrendingController};
+export { getTrendingController, searchController };
--- backend/services/tmdb.service.js
+++ backend/services/tmdb.service.js
@@ -1,24 +1,38 @@
 import axios from "axios";
-import { config } from "dotenv";
-config();
 
 const TMDB_API_KEY = process.env.TMDB_API_KEY;
 const BASE_API_URL = "https://api.themoviedb.org/3/";
 
+async function fetchData(url) {
+  const { data } = await axios.get(url, {
+    headers: {
+      Authorization: `Bearer ${TMDB_API_KEY}`,
+      Accept: "application/json",
+    },
+  });
+
+  return data;
+}
+
 async function getTrending(key, lang) {
   lang = lang === undefined ? "en-US" : lang;
 
-  const { data } = await axios.get(
-    BASE_API_URL + `trending/${key}/week?language=${lang}`,
-    {
-      headers: {
-        Authorization: `Bearer ${TMDB_API_KEY}`,
-        Accept: "application/json",
-      },
-    }
+  const data = await fetchData(
+    BASE_API_URL + `trending/${key}/week?language=${lang}`
   );
 
   return data;
 }
 
-export { getTrending };
+async function searchMulti(query, lang, page = 1) {
+  lang = lang === undefined ? "en-US" : lang;
+  const concodedQuery = encodeURIComponent(query);
+  const data = await fetchData(
+    BASE_API_URL +
+      `search/multi?query=${concodedQuery}&include_adult=false&language=${lang}&page=${page}`
+  );
+
+  return data;
+}
+
+export { getTrending, searchMulti };
