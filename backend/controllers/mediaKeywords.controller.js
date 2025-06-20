47a00eb88f1e17c399522fc7c5e0842302e4fc4b
bassem <182443885+bassemsaeed@users.noreply.github.com>
2025-06-20 05:00:25 +0300

    FIX: MODIFY ENDPOINT TO MATCH MAIN BRANCH

--- backend/controllers/mediaKeywords.controller.js
+++ backend/controllers/mediaKeywords.controller.js
@@ -3,18 +3,15 @@ import { getKeywords } from "../services/tmdb.service.js";
 async function mediaKeywordsController(req, res) {
   const { mediaType, mediaId } = req.params;
 
-
   try {
     const data = await getKeywords(mediaType, mediaId);
     return res.status(200).json(data);
   } catch (error) {
-     return res.status(500).json({
-      status: "Interal server error",
-      message: "Could't fetch the keyowrds -> " + error.message,
+    return res.status(500).json({
+      success: false,
+      message: "Could't fetch the keywords, " + error.message,
     });
   }
 }
 
-
 export { mediaKeywordsController };
-
