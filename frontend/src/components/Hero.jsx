c76028865b69b9dec27e925c990e5926475d23bb
bassem <182443885+bassemsaeed@users.noreply.github.com>
2025-06-28 17:36:01 +0300

    fix:  ui inconsistencies

--- frontend/src/components/Hero.jsx
+++ frontend/src/components/Hero.jsx
@@ -5,7 +5,7 @@ import { ForYou } from "./ForYou";
 const Hero = () => {
   return (
     <div className="h-[calc(100vh-60px)] bg-white dark:bg-neutral-900">
-      <div className="w-full custom-scrollbar h-[100%] overflow-x-hidden dark:border-white grid lg:grid-cols-[1fr_390px] grid-cols-1 lg:grid-rows-1 grid-rows-[800px_300px] px-4 gap-1.5">
+      <div className="w-full custom-scrollbar h-[100%] overflow-hidden dark:border-white grid lg:grid-cols-[1fr_390px] grid-cols-1 lg:grid-rows-1 grid-rows-[800px_300px] px-4 gap-1.5">
         <div className="dark:border-white  grid grid-rows-2 gap-1.5 mt-3">
           {/** For You */}
           <ForYou />
